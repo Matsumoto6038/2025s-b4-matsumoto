2f783ffdeea9360d7a6924569ee3868cd780cbff
松本皓琉 <matsumotohikaru@matsumotoakiraryunoMacBook-Air.local>
2025-06-27 16:59:33 +0900

    管理フォルダを整理

--- test/test_annealing.py
+++ test/test_annealing.py
@@ -55,15 +55,18 @@ import time
 """ annealing """
 L = 2
 h = 0.2
-n_steps = 400
-total_time = 200
+n_steps = 1000
+total_time = 500
 rate = float(total_time/n_steps)
 
 J_holiz, J_vert = MPS.generate_J_array(L=L, seed=12345)
 print(f"J_holiz: {J_holiz}")
 print(f"J_vert: {J_vert}")
-# J_holiz = np.array([0]*L*L)  # 水平方向の相互作用は定数に設定
-# J_vert = np.array([1]*L*L)  # 垂直方向の相互作用はゼロに設定
+J_holiz = np.array([1]*L*L)  # 水平方向の相互作用は定数に設定
+J_vert = np.array([1]*L*L)  # 垂直方向の相互作用はゼロに設定
+for i in range(L):
+    J_holiz[i * L - 1] = 0  
+    J_vert[L * (L-1) + i] = 0
 mpo = MPS.spin_glass_annealing(L=L, h=h, J_holiz=J_holiz, J_vert=J_vert, weight=0)
 mps = MPS.plus(L*L)
 
@@ -88,7 +91,10 @@ for i in range(0,n_steps):
         print(f"Bond dimensions: {cleaned}")
 end = time.time()
 print(f"Annealing time: {end - start:.2f} seconds")
-print(MPS.expval('z', mps, 0))  # M_xの期待値を計算
+# 各ビットの期待値を計算し、小数点以下3桁で表示
+print("Expectation values of each bit:")
+for i in range(L*L):
+    print(f"bit{i}:{MPS.expval('z', mps, 0)[i]:.3f}")
 
 timer = np.linspace(0, total_time, len(result))
 plt.plot(timer, result, label='annealing')
@@ -96,7 +102,7 @@ plt.grid()
 plt.show()
 
 """ 全数探索 """
-# L = 2
+# L = 3
 # def state_to_bit(state: int, L: int):
 #     if state > 2**(L*L) - 1:
 #         raise ValueError(f"state must be in the range [0, {2**(L*L) - 1}].")
@@ -115,17 +121,30 @@ plt.show()
 #         return cost
 #     return cost_func
 
-# J_holiz, J_vert = MPS.generate_J_array(L=L, seed=123)
+# J_holiz, J_vert = MPS.generate_J_array(L=L, seed=12345)
 # print(f"J_holiz: {J_holiz}")
 # print(f"J_vert: {J_vert}")
-# cost_func = make_cost_func(J_holiz, J_vert, 0.5, L)
+# cost_func = make_cost_func(J_holiz, J_vert, 1/L/L, L)
 
 # min_energy = -L*L
 # min_state = 0
 # for i in range(2**(L*L)):
-#     if min_energy < cost_func(i):
+#     if min_energy <= cost_func(i):
 #         min_energy = cost_func(i)
 #         min_state = i
-#         # print(f"state: {i}, min_energy: {-min_energy}")
+#         print(f"state: {i}, min_energy: {-min_energy}")
 # print(f"min_state: {min_state}, min_energy: {-min_energy}")
-# print(f"min_state bit: {state_to_bit(min_state, L)}")
+# print(f"min_state: {state_to_bit(178,L)}")
+# print(f"min_state bit: {state_to_bit(269, L)}")
+
+""" L=3, seed=12345 の場合
+J_holiz: [-1  1  0  1 -1  0 -1 -1  0]
+J_vert: [-1  1  1 -1  1  1  0  0  0]で、
+基底は2重に縮退しており、
+[ 1 -1  1 -1 -1  1  1 -1  1]
+[-1  1  1  1  1 -1 -1  1 -1]
+の2つが最小エネルギー状態。
+アニーリングの結果は、エネルギーはこれと一致するが、得られる状態はこれらの重ね合わせである。
+[-0.238, 0.238, 0.999, 0.238, 0.238, -0.238, -0.238, 0.238, -0.238]
+第二ビットだけ共通なので、期待値は1である。
+"""
